f1e7adfc25a18e8bae387d396622b3709ea89218
TakKokubu <kokubu.tk@gmail.com>
2022-02-07 10:23:12 +0900

    session controllerの修正

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,7 +4,7 @@ module Api
     end
 
     def create
-      user = User.find_by(email: params[:session][:email].downcase)
+      user = User.find_by(email: params[:session][:email])
       if user && user.authenticate(params[:session][:password])
         log_in user
       else
