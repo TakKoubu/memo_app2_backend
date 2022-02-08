6e9c8d4ca1ef8b84e20e84227f8cddb85c35683d
TakKokubu <kokubu.tk@gmail.com>
2022-02-08 14:00:25 +0900

    memoの新規作成

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -1,10 +1,15 @@
 module Api
   class MemosController < ApplicationController
-    before_action :logged_in_user, only: [:create, :destroy]
-    before_action :correct_user,   only: :destroy
+    # before_action :logged_in_user, only: [:create, :destroy]
+    # before_action :correct_user,   only: :destroy
+
+    def index
+      render json: Memo.all
+    end
 
     def create
-      memo = current_user.memos.build(memo_params)
+      user = User.find(1)
+      memo = user.memos.build(memo_params)
       if memo.save
         render json: memo
       else
@@ -22,8 +27,8 @@ module Api
       params.require(:memo).permit(:content)
     end
 
-    def correct_user
-      memo = current_user.memos.find_by(id: params[:id])
-    end
+    # def correct_user
+    #   memo = current_user.memos.find_by(id: params[:id])
+    # end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace 'api' do
     resources :users
-    resources :memos, only: [:create, :destroy]
+    resources :memos, only: [:index, :create, :destroy]
     mount_devise_token_auth_for 'User', at: 'auth'
   end
 end
