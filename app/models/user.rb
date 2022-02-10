b0adece8a9f3b511a35a831374b4d15e543118c3
TakKoubu <48621700+TakKoubu@users.noreply.github.com>
2022-02-10 18:05:29 +0900

    favorite登録機能の作成 (#8)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,16 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true, length: { minimum: 6 } 
+  
   has_many :memos, dependent: :destroy
+  has_many :favorites
+  has_many :favorite_memos, through: :favorites, source: :memo
+
+  def favorite(memo)
+    self.favorites.find_or_create_by(memo_id: memo.id)
+  end
+
+  def favoriting?(memo)
+    self.favorite_memos.include?(memo)
+  end
 end
