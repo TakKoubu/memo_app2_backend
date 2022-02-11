bf357906c02e013d52e5c9c4e8f7bbc50f0ffd02
TakKoubu <48621700+TakKoubu@users.noreply.github.com>
2022-02-11 22:50:43 +0900

    memoをお気に入り解除できるようにする (#10)
    * お気に入り削除機能

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -2,9 +2,16 @@ module Api
  class FavoritesController < ApplicationController
     def create
       # いいねするメモのIDを特定する
-      # current_userがfavoriteメソッドを実行する
       memo = Memo.find(params[:memo_id])
+      # current_userがfavoriteメソッドを実行する
       current_user.favorite(memo)
+    end
+
+    def destroy
+      # いいねを取り消すメモのIDを特定する
+      memo = Memo.find(params[:id])
+      # current_userがfavorite destoryアクションを投げる
+      current_user.unfavorite(memo)
     end
   end
 end
