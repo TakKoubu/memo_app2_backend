77e4b3625c2d69191e18a934317f322c9741f1cf
TakKokubu <kokubu.tk@gmail.com>
2022-02-13 17:43:09 +0900

    指摘箇所の修正

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,15 +1,11 @@
 module Api 
- class FavoritesController < ApplicationController
-    def index
-      render json: User.all
-    end
-    
+ class FavoritesController < ApplicationController    
+
     def create
       # いいねするメモのIDを特定する
       memo = Memo.find(params[:memo_id])
       # current_userがfavoriteメソッドを実行する
       current_user.favorite(memo)
-      render json: favorite_count: 3
     end
 
     def destroy
--- db/migrate/20220213035549_add_favorite_count_to_memos.rb
+++
@@ -1,5 +0,0 @@
-class AddFavoriteCountToMemos < ActiveRecord::Migration[6.0]
-  def change
-    add_column :memos, :favorite_count, :integer
-  end
-end
