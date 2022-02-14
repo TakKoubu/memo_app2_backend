5755377022bf2e25b57f1b3b0fa7cf18f09758a2
TakKoubu <48621700+TakKoubu@users.noreply.github.com>
2022-02-14 14:44:44 +0900

    フロントのいいね数をサーバー側のいいね数と合致させる (#12)
    * フロントのいいね数をサーバー側のいいね数と合致させる

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,18 +1,27 @@
 module Api 
- class FavoritesController < ApplicationController    
-
-    def create
-      # いいねするメモのIDを特定する
-      memo = Memo.find(params[:memo_id])
-      # current_userがfavoriteメソッドを実行する
-      current_user.favorite(memo)
-    end
-
-    def destroy
-      # いいねを取り消すメモのIDを特定する
-      memo = Memo.find(params[:id])
-      # current_userがfavorite destoryアクションを投げる
-      current_user.unfavorite(memo)
-    end
-  end
-end
+  class FavoritesController < ApplicationController    
+ 
+     def create
+       # いいねするメモのIDを特定する
+       memo = Memo.find(params[:memo_id])
+       # current_userがfavoriteメソッドを実行する
+       # Favorite.where(memo_id: 1).count
+       # memo.favorites.count
+       # memo.favorite_count
+       if current_user.favorite(memo)
+         render json: { favorite_count: memo.favorite_count }
+       else
+         render json: favorite.errors, status: 422
+       end
+     end
+ 
+     def destroy
+       # いいねを取り消すメモのIDを特定する
+       memo = Memo.find(params[:id])
+       # current_userがfavorite destoryアクションを投げる
+       if current_user.unfavorite(memo)
+         render json: { favorite_count: memo.favorite_count }
+       end 
+     end
+   end
+ end
